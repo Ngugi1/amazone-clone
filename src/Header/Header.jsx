8b7ddbf67b980499ed0cf99e25ee99795112286b
Samuel <samuel.ngugi@vub.ac.be>
2021-02-25 10:00:37 +0300

    Add mgema digital header component

---
+++ src/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBeer,faSearch} from '@fortawesome/free-solid-svg-icons'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="header_logo_container">
+                    <FontAwesomeIcon icon={faBeer} size="1x" color="yellow" pull="left" transform={{ rotate: 200 }}/>
+                    <FontAwesomeIcon icon={faBeer} size="1x" color="green" transform={{ rotate: -20 }}/>
+                    <span className="header_app_name">mgema.digital</span>
+
+                </div>
+                <div className="header_search">
+                    <input className="header_search_input" type="text" placeholder="search for drinks" />
+                    <FontAwesomeIcon icon={faSearch} color="black" className="header_search_button" />
+                </div>
+            </div>
+        );
+        }
+    }
+
+    export default Header;
